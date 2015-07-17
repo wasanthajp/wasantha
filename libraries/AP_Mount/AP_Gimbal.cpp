e91154e9041a001dc7cc8eb488746c788e720be1
Jonathan Challinger <mr.challinger@gmail.com>
2015-07-16 19:09:38 -0700

    AP_Gimbal: constrain rate demand to 400 deg/s

--- libraries/AP_Mount/AP_Gimbal.cpp
+++ libraries/AP_Mount/AP_Gimbal.cpp
@@ -147,6 +147,11 @@ void AP_Gimbal::update_state()
         gimbalRateDemVec += getGimbalRateDemVecGyroBias();
     }
 
+    float gimbalRateDemVecLen = gimbalRateDemVec.length();
+    if (gimbalRateDemVecLen > radians(400)) {
+        gimbalRateDemVec *= radians(400)/gimbalRateDemVecLen;
+    }
+
     update_joint_angle_est();
 }
 
