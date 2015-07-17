ac48801a5518dce8983c2f61de8bd95671af4244
Jonathan Challinger <mr.challinger@gmail.com>
2015-07-17 13:48:29 -0700

    AP_Gimbal: always add gyro bias to rate demand

--- libraries/AP_Mount/AP_Gimbal.cpp
+++ libraries/AP_Mount/AP_Gimbal.cpp
@@ -144,13 +144,14 @@ void AP_Gimbal::update_state()
     gimbalRateDemVec += getGimbalRateDemVecYaw(quatEst);
     gimbalRateDemVec += getGimbalRateDemVecTilt(quatEst);
     gimbalRateDemVec += getGimbalRateDemVecForward(quatEst);
-        gimbalRateDemVec += getGimbalRateDemVecGyroBias();
     }
 
     float gimbalRateDemVecLen = gimbalRateDemVec.length();
     if (gimbalRateDemVecLen > radians(400)) {
         gimbalRateDemVec *= radians(400)/gimbalRateDemVecLen;
     }
+
+    gimbalRateDemVec += getGimbalRateDemVecGyroBias();
 
     update_joint_angle_est();
 }
