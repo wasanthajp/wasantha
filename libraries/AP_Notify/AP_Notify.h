0fa9867e946fa71997ddef1281e667cc068bc186
Randy Mackay <rmackay9@yahoo.com>
2015-09-11 17:44:11 +0900

    Copter: enable oreoled -- do not merge

--- libraries/AP_Notify/AP_Notify.h
+++ libraries/AP_Notify/AP_Notify.h
@@ -33,7 +33,7 @@
 #include "OreoLED_PX4.h"
 
 #ifndef OREOLED_ENABLED
- # define OREOLED_ENABLED   0   // set to 1 to enable OreoLEDs
+ # define OREOLED_ENABLED   1   // set to 1 to enable OreoLEDs
 #endif
 
 class AP_Notify
