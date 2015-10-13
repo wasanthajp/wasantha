2d56be511f2f9f07a88ed26249e181ff5ca9c123
Randy Mackay <rmackay9@yahoo.com>
2015-10-13 21:03:19 +0900

    Copter: arming_checks simplified by removing references

--- ArduCopter/arming_checks.h
+++ ArduCopter/arming_checks.h
@@ -19,16 +19,8 @@
 class AP_Arming_Copter : public AP_Arming
 {
 public:
-    AP_Arming_Copter(const AP_AHRS &ahrs_ref, const AP_Baro &baro, Compass &compass, const enum HomeState &home_state,
-            const AP_InertialNav_NavEKF &inav, const AP_Vehicle::MultiCopter &aparm,
-            RC_Channel*& channel_roll, RC_Channel*& channel_pitch, RC_Channel*& channel_throttle, RC_Channel*& channel_yaw) :
-        AP_Arming(ahrs_ref, baro, compass, home_state),
-        _inav(inav),
-        _aparm(aparm),
-        _channel_roll(channel_roll),
-        _channel_pitch(channel_pitch),
-        _channel_throttle(channel_throttle),
-        _channel_yaw(channel_yaw)
+    AP_Arming_Copter(const AP_AHRS &ahrs_ref, const AP_Baro &baro, Compass &compass, const enum HomeState &home_state) :
+        AP_Arming(ahrs_ref, baro, compass, home_state)
     {
             AP_Param::setup_object_defaults(this, var_info);
     }
@@ -45,14 +37,6 @@ public:
 
     // var_info for holding Parameter information
     static const struct AP_Param::GroupInfo var_info[];
-
-private:
-    const AP_InertialNav_NavEKF &_inav;
-    const AP_Vehicle::MultiCopter &_aparm;
-    RC_Channel*& _channel_roll;
-    RC_Channel*& _channel_pitch;
-    RC_Channel*& _channel_throttle;
-    RC_Channel*& _channel_yaw;
 };
 
 #endif //  __ARMING_CHECKS_H__
