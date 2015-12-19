d326c00d438a93d41da2f9e7be14dd704cc372a5
Randy Mackay <rmackay9@yahoo.com>
2015-12-19 12:37:22 +0900

    MotorsMatrix: use get_hover_throttle_as_high_end_pct

--- libraries/AP_Motors/AP_MotorsMatrix.cpp
+++ libraries/AP_Motors/AP_MotorsMatrix.cpp
@@ -239,8 +239,8 @@ void AP_MotorsMatrix::output_armed_stabilizing()
     //      We will choose #1 (the best throttle for yaw control) if that means reducing throttle to the motors (i.e. we favour reducing throttle *because* it provides better yaw control)
     //      We will choose #2 (a mix of pilot and hover throttle) only when the throttle is quite low.  We favour reducing throttle instead of better yaw control because the pilot has commanded it
 
-    float hover_thrust = _hover_out / 1000.0f;
-    throttle_thrust_best_rpy = MIN(0.5f - (rpy_low+rpy_high)/2.0, MAX(throttle_thrust, throttle_thrust*MAX(0.0f,1.0f-_throttle_rpy_mix)+hover_thrust*_throttle_rpy_mix));
+    float throttle_thrust_hover = get_hover_throttle_as_high_end_pct();
+    throttle_thrust_best_rpy = MIN(0.5f - (rpy_low+rpy_high)/2.0, MAX(throttle_thrust, throttle_thrust*MAX(0.0f,1.0f-_throttle_rpy_mix)+throttle_thrust_hover*_throttle_rpy_mix));
 
     // calculate amount of yaw we can fit into the throttle range
     // this is always equal to or less than the requested yaw from the pilot or rate controller
