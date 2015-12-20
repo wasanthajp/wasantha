0ea29f87c60a34bce5cb73eed42b1433062e6498
Randy Mackay <rmackay9@yahoo.com>
2015-12-20 12:48:26 +0900

    MotorsMulticopter: debug changes -- do not merge

--- libraries/AP_Motors/AP_MotorsMulticopter.cpp
+++ libraries/AP_Motors/AP_MotorsMulticopter.cpp
@@ -736,14 +736,22 @@ void AP_MotorsMulticopter::output_to_motors()
 
     static uint16_t counter = 0;
     counter++;
-    if (counter > 400) {
+    if (counter > 100) {
         counter = 0;
-        hal.console->printf("SM:%d T0:%4.2f T1:%4.2f T2:%4.2f T3:%4.2f\n",
+        hal.console->printf("SM:%d M0:%4.2f 1:%4.2f 2:%4.2f 3:%4.2f TTM:%4.2f RPYM:%4.2f T0:%4.2f T1:%4.2f T2:%4.2f T3:%4.2f TI:%4.2f\n",
                 (int)_multicopter_flags.spool_mode,
-                (float)_thrust_rpyt_out[0],
-                (float)_thrust_rpyt_out[1],
-                (float)_thrust_rpyt_out[2],
-                (float)_thrust_rpyt_out[3]);
+                (double)motor_out[0],
+                (double)motor_out[1],
+                (double)motor_out[2],
+                (double)motor_out[3],
+                (double)_throttle_thrust_max,
+                (double)_throttle_rpy_mix,
+                (double)_thrust_rpyt_out[0],
+                (double)_thrust_rpyt_out[1],
+                (double)_thrust_rpyt_out[2],
+                (double)_thrust_rpyt_out[3],
+                (double)_throttle_control_input
+                );
     }
 }
 
