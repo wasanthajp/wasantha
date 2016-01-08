3381b72aadbb8b0cb348677b48bf2beecc1e357e
Randy Mackay <rmackay9@yahoo.com>
2016-01-08 18:49:39 +0900

    HAL_Empty: fake pwm output for testing -- DO NOT MERGE

--- libraries/AP_HAL_Empty/RCOutput.h
+++ libraries/AP_HAL_Empty/RCOutput.h
@@ -13,6 +13,7 @@ class Empty::RCOutput : public AP_HAL::RCOutput {
     void     write(uint8_t ch, uint16_t period_us);
     uint16_t read(uint8_t ch);
     void     read(uint16_t* period_us, uint8_t len);
+    int16_t _pwm_output[16];
 };
 
 #endif // __AP_HAL_EMPTY_RCOUTPUT_H__
