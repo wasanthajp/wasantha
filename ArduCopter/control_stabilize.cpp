7ce7481c3e0f3326ad3c75015d1da02d215dd762
Randy Mackay <rmackay9@yahoo.com>
2016-01-22 11:51:58 +0900

    Copter: merge with stabilize set motor spool state

--- ArduCopter/control_stabilize.cpp
+++ ArduCopter/control_stabilize.cpp
@@ -29,11 +29,7 @@ void Copter::stabilize_run()
 
     // if not armed set throttle to zero and exit immediately
     if (!motors.armed() || ap.throttle_zero || !motors.get_interlock()) {
-        if (!motors.armed() || !motors.get_interlock()) {
-            motors.set_desired_spool_state(AP_MotorsMulticopter::DESIRED_SHUT_DOWN);
-        } else {
-            motors.set_desired_spool_state(AP_MotorsMulticopter::DESIRED_SPIN_WHEN_ARMED);
-        }
+        motors.set_desired_spool_state(AP_MotorsMulticopter::DESIRED_SPIN_WHEN_ARMED);
         attitude_control.set_throttle_out_unstabilized(0,true,g.throttle_filt);
         // slow start if landed
         if (ap.land_complete) {
