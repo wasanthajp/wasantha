654c74510bf1727a25c7eba801f54b1096d35dad
Randy Mackay <rmackay9@yahoo.com>
2016-01-22 11:51:13 +0900

    Copter: merge with poshold set motor spool state

--- ArduCopter/control_poshold.cpp
+++ ArduCopter/control_poshold.cpp
@@ -142,11 +142,7 @@ void Copter::poshold_run()
 
     // if not auto armed or motor interlock not enabled set throttle to zero and exit immediately
     if (!motors.armed() || !ap.auto_armed || !motors.get_interlock()) {
-        if (!motors.armed() || !motors.get_interlock()) {
-            motors.set_desired_spool_state(AP_MotorsMulticopter::DESIRED_SHUT_DOWN);
-        } else {
-            motors.set_desired_spool_state(AP_MotorsMulticopter::DESIRED_SPIN_WHEN_ARMED);
-        }
+        motors.set_desired_spool_state(AP_MotorsMulticopter::DESIRED_SPIN_WHEN_ARMED);
         wp_nav.init_loiter_target();
         attitude_control.set_throttle_out_unstabilized(0,true,g.throttle_filt);
         pos_control.relax_alt_hold_controllers(get_throttle_pre_takeoff(channel_throttle->control_in)-throttle_average);
