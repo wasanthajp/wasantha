bf4fb1c1b088c890ce487438dd9dc0c32244d3bd
Randy Mackay <rmackay9@yahoo.com>
2016-01-22 11:47:06 +0900

    Copter: merge with acro set motor spool state

--- ArduCopter/control_acro.cpp
+++ ArduCopter/control_acro.cpp
@@ -27,12 +27,8 @@ void Copter::acro_run()
     float pilot_throttle_scaled;
 
     // if not armed set throttle to zero and exit immediately
-    if(!motors.armed() || ap.throttle_zero) {
-        if (!motors.armed()) {
-            motors.set_desired_spool_state(AP_MotorsMulticopter::DESIRED_SHUT_DOWN);
-        } else {
-            motors.set_desired_spool_state(AP_MotorsMulticopter::DESIRED_SPIN_WHEN_ARMED);
-        }
+    if (!motors.armed() || ap.throttle_zero || !motors.get_interlock()) {
+        motors.set_desired_spool_state(AP_MotorsMulticopter::DESIRED_SPIN_WHEN_ARMED);
         attitude_control.set_throttle_out_unstabilized(0,true,g.throttle_filt);
         // slow start if landed
         if (ap.land_complete) {
