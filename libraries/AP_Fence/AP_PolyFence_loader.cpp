2c18227de397c17e8976f266afa60527ca93151a
Randy Mackay <rmackay9@yahoo.com>
2016-06-22 13:37:41 +0900

    AP_Polygon_loader: first draft

--- libraries/AP_Fence/AP_PolyFence_loader.cpp
+++ libraries/AP_Fence/AP_PolyFence_loader.cpp
@@ -1,246 +1,115 @@
-#include "AP_PolyFence.h"
+#include "AP_PolyFence_loader.h"
 
 extern const AP_HAL::HAL& hal;
 
-#define MIN_GEOFENCE_POINTS 5 // 3 to define a minimal polygon (triangle)
-                              // + 1 for return point and +1 for last
-                              // pt (same as first)
-
-const AP_Param::GroupInfo AP_PolyFence::var_info[] = {
-    // @Param: _TOTAL
-    // @DisplayName: Fence total number of points
-    // @Description: Number of geofence points currently loaded
-    // @User: Advanced
-    AP_GROUPINFO("_TOTAL", 1, AP_PolyFence, g.fence_total, 0),
-
-    AP_GROUPEND
-};
-
 static const StorageAccess fence_storage(StorageManager::StorageFence);
 
-AP_PolyFence::AP_PolyFence() :
+AP_PolyFence_loader::AP_PolyFence_loader()
 {
-    // initialise parameter defaults
-    AP_Param::setup_object_defaults(this, var_info);
-}
-
-// enable or disable fence
-bool AP_PolyFence::set_enabled(bool enable)
-{
-    _enabled = enable;
-    return enabled();
-}
-
-// check if fence is enabled
-bool AP_PolyFence::enabled() const
-{
-    // check if state has been created
-    if (geofence_state == NULL) {
-        return false;
-    }
-
-    // check all points have been loaded from eeprom and validated
-    if (!geofence_state->boundary_uptodate || !geofence_state->boundary_valid) {
-        return false;
-    }
-
-    return _enabled;
-}
-
-/*
- *  fence boundaries fetch/store
- */
-Vector2l AP_PolyFence::load_point_from_eeprom(uint16_t i)
-{
-    Vector2l ret;
-
-    // return empty Vector if point is invalid
-    if (i > _total || i >= max_points()) {
-        return Vector2l(0,0);
-    }
-
-    // read fence point
-    ret.x = fence_storage.read_uint32(i * sizeof(Vector2l));
-    ret.y = fence_storage.read_uint32(i * sizeof(Vector2l) + 4);
-
-    return ret;
-}
-
-// save a fence point to eeprom
-void AP_PolyFence::save_point_to_eeprom(uint16_t i, Vector2l &point)
-{
-    if (i >= _total || i >= max_points()) {
-        // not allowed
-        return;
-    }
-
-    // write point to eeprom
-    fence_storage.write_uint32(i * sizeof(Vector2l), point.x);
-    fence_storage.write_uint32(i * sizeof(Vector2l)+4, point.y);
-
-    // flag that we need to check boundary is complete
-    if (geofence_state != NULL) {
-        geofence_state->boundary_uptodate = false;
-    }
-}
-
-/*
- *  allocate and fill the geofence state structure
- */
-void AP_PolyFence::load()
-{
-    // return immediately if geofence is setup correctly
-    if (geofence_state != NULL && geofence_state->boundary_uptodate) {
-        return;
-    }
-
-    // allocate memory if required
-    if (geofence_state == NULL) {
-        uint16_t boundary_size = sizeof(Vector2l) * max_points();
-        if (hal.util->available_memory() < 100 + boundary_size + sizeof(struct GeofenceState)) {
-            // too risky to enable as we could run out of stack
-            goto failed;
-        }
-
-        // allocate state
-        geofence_state = (struct GeofenceState *)calloc(1, sizeof(struct GeofenceState));
-        if (geofence_state == NULL) {
-            // not much we can do here except disable it
-            goto failed;
-        }
-
-        // allocate boundary points
-        geofence_state->boundary = (Vector2l *)calloc(1, boundary_size);
-        if (geofence_state->boundary == NULL) {
-            free(geofence_state);
-            geofence_state = NULL;
-            goto failed;
-        }
-
-        // initialise state
-        geofence_state->breached_polygon = false;
-        geofence_state->breach_count = 0;
-        geofence_state->breach_type = FENCE_BREACH_NONE;
-        geofence_state->breach_time = 0;
-        geofence_state->boundary_num_points = 0;
-        geofence_state->boundary_uptodate = false;
-        geofence_state->boundary_valid = false;
-    }
-
-    // exit immediately if no points in eeprom
-    if (_total <= 0) {
-        _total.set(0);
-        return;
-    }
-
-    // load points from eeprom into boundary array
-    if (!geofence_state->boundary_uptodate) {
-        for (uint16_t i=0; i<_total; i++) {
-            geofence_state->boundary[i] = load_point_from_eeprom(i);
-        }
-        geofence_state->boundary_num_points = i;
-        geofence_state->boundary_uptodate = true;
-        geofence_state->boundary_valid = false;
-
-        // point 1 and last point must be the same.  Note: 0th point is reserved as the return point
-        if (!Polygon_complete(&geofence_state->boundary[1], geofence_state->boundary_num_points-1)) {
-            goto failed;
-        }
-
-        // check return point is within the fence
-        if (Polygon_outside(geofence_state->boundary[0], &geofence_state->boundary[1], geofence_state->boundary_num_points-1)) {
-            goto failed;
-        }
-
-        // mark boundary as valid
-        geofence_state->boundary_valid = false;
-    }
-
-    geofence_state->fence_triggered = false;
-
-    gcs_send_text(MAV_SEVERITY_INFO,"Geofence loaded");
-    gcs_send_message(MSG_FENCE_STATUS);
-    return;
-
-failed:
-    gcs_send_text(MAV_SEVERITY_WARNING,"Geofence setup error");
-}
-
-/*
- * return true if a geo-fence has been uploaded and
- * FENCE_ACTION is 1 (not necessarily enabled)
- */
-bool AP_PolyFence::present() const
-{
-    //require at least a return point and a triangle
-    //to define a geofence area:
-    if (g.fence_action == FENCE_ACTION_NONE || g.fence_total < MIN_GEOFENCE_POINTS) {
-        return false;
-    }
-    return true;
-}
-
-/*
- *
- */
-void AP_PolyFence::send_status(mavlink_channel_t chan)
-{
-    if (geofence_enabled() && geofence_state != NULL) {
-        mavlink_msg_fence_status_send(chan,
-                                      (int8_t)geofence_state->fence_triggered,
-                                      geofence_state->breach_count,
-                                      geofence_state->breach_type,
-                                      geofence_state->breach_time);
-    }
-}
-
-/*
-  return true if geofence has been breached
- */
-bool AP_PolyFence::breached()
-{
-    return geofence_state ? geofence_state->fence_triggered : false;
-}
-
-
-/*
- *  check if we have breached the polygon fence
- */
-void AP_PolyFence::check(const Location& loc)
-{
-    /* allocate the geo-fence state if need be */
-    if (geofence_state == NULL || !geofence_state->boundary_uptodate) {
-        load();
-        if (!geofence_enabled()) {
-            // may have been disabled by load
-            return;
-        }
-    }
-
-    // check if vehicle is within the polygon
-    Vector2l location(loc.lat, loc.lng);
-    bool outside = Polygon_outside(location, &geofence_state->boundary[1], geofence_state->boundary_num_points-1);
-
-    // check for change in breached state
-    if (outside != geofence_state->breached_polygon) {
-        geofence_state->breached_polygon = outside;
-        if (outside) {
-            geofence_state->breach_type = FENCE_BREACH_BOUNDARY;
-            geofence_state->breach_count++;
-            geofence_state->breach_time = AP_HAL::millis();
-            gcs_send_text(MAV_SEVERITY_NOTICE,"Geofence triggered");
-        } else {
-            geofence_state->breach_type = FENCE_BREACH_NONE;
-            gcs_send_text(MAV_SEVERITY_INFO,"Geofence OK");
-        }
-        gcs_send_message(MSG_FENCE_STATUS);
-    }
 }
 
 /*
   maximum number of fencepoints
  */
-uint8_t AP_PolyFence::max_points()
+uint8_t AP_PolyFence_loader::max_points()
 {
     return MIN(255U, fence_storage.size() / sizeof(Vector2l));
+}
+
+// load boundary point from eeprom, returns true on successful load
+bool AP_PolyFence_loader::load_point_from_eeprom(uint16_t i, Vector2l& point)
+{
+    // sanity check index
+    if (i >= max_points()) {
+        return false;
+    }
+
+    // read fence point
+    point.x = fence_storage.read_uint32(i * sizeof(Vector2l));
+    point.y = fence_storage.read_uint32(i * sizeof(Vector2l) + 4);
+    return true;
+}
+
+// save a fence point to eeprom, returns true on successful save
+bool AP_PolyFence_loader::save_point_to_eeprom(uint16_t i, const Vector2l &point)
+{
+    // sanity check index
+    if (i >= max_points()) {
+        return false;
+    }
+
+    // write point to eeprom
+    fence_storage.write_uint32(i * sizeof(Vector2l), point.x);
+    fence_storage.write_uint32(i * sizeof(Vector2l)+4, point.y);
+    return true;
+}
+
+// validate array of boundary points (expressed as either floats or long ints)
+//   contains_return_point should be true for plane which stores the return point as the first point in the array
+//   returns true if boundary is valid
+bool AP_PolyFence_loader::boundary_valid(uint16_t num_points, const Vector2l& points[], bool contains_return_point)
+{
+    // start from 2nd point if boundary contains return point (as first point)
+    uint8_t start_num = contains_return_point ? 1 : 0;
+
+    // a boundary requires at least 4 point (a triangle and last point equals first)
+    if (num_points < start_num + 4) {
+        return false;
+    }
+
+    // point 1 and last point must be the same.  Note: 0th point is reserved as the return point
+    if (!Polygon_complete(points[start_num], num_points-1)) {
+        return false;
+    }
+
+    // check return point is within the fence
+    if (contains_return_point && Polygon_outside(points[0], &points[1], num_points-1)) {
+        return false;
+    }
+
+    return true;
+}
+
+bool AP_PolyFence_loader::boundary_valid(uint16_t num_points, const Vector2f& points[], bool contains_return_point)
+{
+    // start from 2nd point if boundary contains return point (as first point)
+    uint8_t start_num = contains_return_point ? 1 : 0;
+
+    // a boundary requires at least 4 point (a triangle and last point equals first)
+    if (num_points < start_num + 4) {
+        return false;
+    }
+
+    // point 1 and last point must be the same.  Note: 0th point is reserved as the return point
+    if (!Polygon_complete(points[start_num], num_points-1)) {
+        return false;
+    }
+
+    // check return point is within the fence
+    if (contains_return_point && Polygon_outside(points[0], &points[1], num_points-1)) {
+        return false;
+    }
+
+    return true;
+}
+
+// check if a location (expressed as either floats or long ints) is within the boundary
+//   contains_return_point should be true for plane which stores the return point as the first point in the array
+//   returns true if location is outside the boundary
+bool AP_PolyFence_loader::boundary_breached(const Vector2l& location, uint16_t num_points, const Vector2l& points[], bool contains_return_point)
+{
+    // start from 2nd point if boundary contains return point (as first point)
+    uint8_t start_num = contains_return_point ? 1 : 0;
+
+    // check location is within the fence
+    return Polygon_outside(location, &points[start_num], num_points-1);
+}
+
+bool AP_PolyFence_loader::boundary_breached(const Vector2f& location, uint16_t num_points, const Vector2f& points[], bool contains_return_point)
+{
+    // start from 2nd point if boundary contains return point (as first point)
+    uint8_t start_num = contains_return_point ? 1 : 0;
+
+    // check location is within the fence
+    return Polygon_outside(location, &points[start_num], num_points-1);
 }
--- libraries/AP_Fence/AP_PolyFence_loader.h
+++ libraries/AP_Fence/AP_PolyFence_loader.h
@@ -1,73 +1,36 @@
 #pragma once
 
-#include <stdint.h>
-
+#include <AP_Common/AP_Common.h>
 #include <AP_Math/AP_Math.h>
-#include <GCS_MAVLink/GCS.h>
 
-class AP_PolyFence
+class AP_PolyFence_loader
 {
 
 public:
 
-    AP_PolyFence();
+    // constructor
+    AP_PolyFence_loader();
 
-    // enable or disable fence
-    bool set_enabled(bool enable);
-
-    // check if fence is enabled
-    bool enabled() const;
-
-    // check for breaches
-    void check();
-
-    // returns true if last check found vehicle has breached the fence
-    bool breached();
-
-    // parameter table
-    static const struct AP_Param::GroupInfo        var_info[];
-
-protected:
-
-    // maximum number of fence points we can store
+    // maximum number of fence points we can store in eeprom
     uint8_t max_points();
 
-    // allocate memory and load fence points from eeprom
-    void load();
+    // load boundary point from eeprom, returns true on successful load
+    bool load_point_from_eeprom(uint16_t i, Vector2l& point);
 
-    // load or save individual points to or from eeprom
-    Vector2l load_point_from_eeprom(uint16_t i);
-    void save_point_to_eeprom(uint16_t i, Vector2l &point);
+    // save a fence point to eeprom, returns true on successful save
+    bool save_point_to_eeprom(uint16_t i, const Vector2l &point);
 
-    // parameters
-    AP_Int8 _total;
+    // validate array of boundary points (expressed as either floats or long ints)
+    //   contains_return_point should be true for plane which stores the return point as the first point in the array
+    //   returns true if boundary is valid
+    bool boundary_valid(uint16_t num_points, const Vector2l& points[], bool contains_return_point);
+    bool boundary_valid(uint16_t num_points, const Vector2f& points[], bool contains_return_point);
 
-    /*
-     *  The state of geo-fencing. This structure is dynamically allocated
-     *  the first time it is used. This means we only pay for the pointer
-     *  and not the structure on systems where geo-fencing is not being
-     *  used.
-     *
-     *  We store a copy of the boundary in memory as we need to access it
-     *  very quickly at runtime
-     */
-    struct GeofenceState {
-        bool breached_polygon;          // true if vehicle is currently outside the polygon fence
-        uint16_t breach_count;
-        uint8_t breach_type;
-        uint32_t breach_time;
-
-        // boundary state
-        uint8_t boundary_num_points;    // number of boundary points in boundary array
-        bool boundary_uptodate;         // boundary array is completely loaded with all points from eeprom
-        bool boundary_valid;            // boundary is a complete polygon
-        Vector2l *boundary;             // array of boundary points.  Note: point 0 is the return point
-    } *geofence_state;
-
-    bool _enabled = false;
-
-    virtual void gcs_send_text(MAV_SEVERITY severity, const char *str) = 0;
-    virtual void gcs_send_message(enum ap_message id) = 0;
+    // check if a location (expressed as either floats or long ints) is within the boundary
+    //   contains_return_point should be true for plane which stores the return point as the first point in the array
+    //   returns true if location is outside the boundary
+    bool boundary_breached(const Vector2l& location, uint16_t num_points, const Vector2l& points[], bool contains_return_point);
+    bool boundary_breached(const Vector2f& location, uint16_t num_points, const Vector2f& points[], bool contains_return_point);
 
 };
 
