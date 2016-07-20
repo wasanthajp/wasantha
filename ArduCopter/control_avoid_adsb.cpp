b1797affb2a15d8ccd44007a7f6f13f313472a3f
Randy Mackay <rmackay9@yahoo.com>
2016-07-20 20:46:24 +0900

    Copter: control_avoid_adsb uses guided mode controllers

--- ArduCopter/control_avoid_adsb.cpp
+++ ArduCopter/control_avoid_adsb.cpp
@@ -11,28 +11,10 @@
 // initialise avoid_adsb controller
 bool Copter::avoid_adsb_init(const bool ignore_checks)
 {
-    if (!(position_ok() || ignore_checks)) {
-        return false;
-    }
-
-    // initialise waypoint and spline controller
-    wp_nav.wp_and_spline_init();
-
-    // initialise wpnav to stopping point at current altitude
-    // To-Do: set to current location if disarmed?
-    // To-Do: set to stopping point altitude?
-    Vector3f stopping_point;
-    stopping_point.z = inertial_nav.get_altitude();
-    wp_nav.get_wp_stopping_point_xy(stopping_point);
-
-    if (!wp_nav.set_wp_destination(stopping_point, false)) {
-        return false;
-    }
-
-    // initialise yaw
-    set_auto_yaw_mode(get_default_auto_yaw_mode(false));
-
-    return true;
+    // re-use guided mode
+    // Note: this is safe from interference from GCSs and companion computer's whose guided mode
+    //       position and velocity requests will be ignored while the vehicle is not in guided mode
+    return guided_init(ignore_checks);
 }
 
 // avoid_set_destination - sets AVOID mode's target destination
@@ -44,12 +26,20 @@ bool Copter::avoid_adsb_set_destination(const Vector3f& destination)
         return false;
     }
 
-    if (!wp_nav.set_wp_destination(destination, false)) {
+    // re-use guided mode's position controller
+    return guided_set_destination(destination);
+}
+
+bool Copter::avoid_adsb_set_velocity(const Vector3f& velocity_neu)
+{
+    // check flight mode
+    if (control_mode != AVOID_ADSB) {
         return false;
     }
 
-    // log target
-    // Log_Write_GuidedTarget(guided_mode, destination, Vector3f());
+
+    // re-use guided mode's velocity controller
+    guided_set_velocity(velocity_neu);
     return true;
 }
 
@@ -67,45 +57,8 @@ bool Copter::avoid_adsb_set_target_alt(int32_t target_alt_cm)
 // runs the AVOID_ADSB controller
 void Copter::avoid_adsb_run()
 {
-    // if not auto armed or motors not enabled set throttle to zero and exit immediately
-    if (!motors.armed() || !ap.auto_armed || !motors.get_interlock() || ap.land_complete) {
-#if FRAME_CONFIG == HELI_FRAME  // Helicopters always stabilize roll/pitch/yaw
-        // call attitude controller
-        attitude_control.input_euler_angle_roll_pitch_euler_rate_yaw(0, 0, 0, get_smoothing_gain());
-        attitude_control.set_throttle_out(0,false,g.throttle_filt);
-#else
-        motors.set_desired_spool_state(AP_Motors::DESIRED_SPIN_WHEN_ARMED);
-        // multicopters do not stabilize roll/pitch/yaw when disarmed
-        attitude_control.set_throttle_out_unstabilized(0,true,g.throttle_filt);
-#endif
-        return;
-    }
-
-    // process pilot's yaw input
-    float target_yaw_rate = 0;
-    if (!failsafe.radio) {
-        // get pilot's desired yaw rate
-        target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->get_control_in());
-        if (!is_zero(target_yaw_rate)) {
-            set_auto_yaw_mode(AUTO_YAW_HOLD);
-        }
-    }
-
-    // set motors to full range
-    motors.set_desired_spool_state(AP_Motors::DESIRED_THROTTLE_UNLIMITED);
-
-    // run waypoint controller
-    failsafe_terrain_set_status(wp_nav.update_wpnav());
-
-    // call z-axis position controller (wpnav should have already updated it's alt target)
-    pos_control.update_z_controller();
-
-    // call attitude controller
-    if (auto_yaw_mode == AUTO_YAW_HOLD) {
-        // roll & pitch from waypoint controller, yaw rate from pilot
-        attitude_control.input_euler_angle_roll_pitch_euler_rate_yaw(wp_nav.get_roll(), wp_nav.get_pitch(), target_yaw_rate, get_smoothing_gain());
-    }else{
-        // roll, pitch from waypoint controller, yaw heading from auto_heading()
-        attitude_control.input_euler_angle_roll_pitch_yaw(wp_nav.get_roll(), wp_nav.get_pitch(), get_auto_heading(), true, get_smoothing_gain());
-    }
+    // re-use guided mode's velocity controller
+    // Note: this is safe from interference from GCSs and companion computer's whose guided mode
+    //       position and velocity requests will be ignored while the vehicle is not in guided mode
+    guided_run();
 }
