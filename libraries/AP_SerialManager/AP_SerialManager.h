084058c77d55167bd24c338e2ef324f11498f589
Randy Mackay <rmackay9@yahoo.com>
2016-10-24 20:58:29 +0900

    SerialManager: add beacon to list of protocols

--- libraries/AP_SerialManager/AP_SerialManager.h
+++ libraries/AP_SerialManager/AP_SerialManager.h
@@ -83,6 +83,7 @@ public:
         SerialProtocol_Lidar = 9,
         SerialProtocol_FrSky_SPort_Passthrough = 10, // FrSky SPort Passthrough (OpenTX) protocol (X-receivers)
         SerialProtocol_Lidar360 = 11,
+        SerialProtocol_Beacon = 12
     };
 
     // Constructor
